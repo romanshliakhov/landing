3b8d886a8b1d9bff78c640401af07131e9aa7f63
Roman <romanshliakhov@gmail.com>
2022-03-29 22:37:04 +0300

    added section and mobile menu

--- source/js/common.js
+++ source/js/common.js
@@ -1,5 +1,42 @@
+// burger menu
+let menuBtn = document.querySelector('.burger-menu');
+let menu = document.querySelector('.mobile-menu');
+let disableScroll = document.querySelector('body');
 
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+  disableScroll.classList.toggle('body-scroll');
+});
 
+// select lang
+let select = function () {
+	let selectHeader = document.querySelectorAll('.select__header');
+	let selectItem = document.querySelectorAll('.select__item');
+  
+	selectHeader.forEach(item => {
+	  item.addEventListener('click', selectToggle);
+	});
+  
+	selectItem.forEach(item => {
+	  item.addEventListener('click', selectChoose);
+	});
+  
+	function selectToggle() {
+	  this.parentElement.classList.toggle('is-active');
+	}
+  
+	function selectChoose() {
+	  let folder = this.innerHTML,
+		  select = this.closest('.select'),
+		  currentFolder = select.querySelector('.select__current');
+		  currentFolder.innerHTML = folder;
+	  	  select.classList.remove('is-active');		
+	}
+};
+  
+select();
+  
 
 
 
